47af9005f8662d67423c309723159b97258160d1
juliusmarminge <julius0216@outlook.com>
2023-05-14 20:58:21 +0200

    comment

--- frontend/app/contants.ts
+++ frontend/app/contants.ts
@@ -2,6 +2,6 @@
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL ?? "http://127.0.0.1:8000";
 
-// No sophisticated auth here, just a plain user object in LS
+// No sophisticated auth here, just a plain user object in LS and client-side cookie
 export const LOCALSTORAGE_KEY = "active_user";
 export const COOKIE_NAME = "x-spotipy-user";
