aefe84eb7aa222bb4c511c5a984bf3c08ea4e5e0
juliusmarminge <julius0216@outlook.com>
2023-05-14 22:51:54 +0200

    make playlist navigable

--- frontend/components/button.tsx
+++ frontend/components/button.tsx
@@ -9,7 +9,7 @@ export const Button = React.forwardRef<
   return (
     <button
       className={twMerge(
-        "inline-flex items-center justify-center h-10 py-2 px-4 rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-accent focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background",
+        "inline-flex items-center justify-center h-10 py-2 px-4 rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-foreground-muted focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background",
         "bg-primary text-primary-foreground hover:bg-primary/90",
         className
       )}
